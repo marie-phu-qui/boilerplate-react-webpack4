c8c8ef7e9630beaf3474103776177a0c9bf629bb
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2019-01-07 18:46:59 +1300

    a massive commit of a ready boilerplate webpack4, react 16, babel7

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Boilerplate ready !!
+            </h1>
+
+            <p>
+                go go go
+            </p>
+        </div>
+    )
+  }
+
+export default App
